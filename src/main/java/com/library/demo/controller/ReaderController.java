16009f8f193cbc0bf28bfef4fc25fe8066ca278f
alxzhtkv <alxzhtkv@gmail.com>
2023-04-29 20:47:58 +0300

    added user tracking

--- src/main/java/com/library/demo/controller/ReaderController.java
+++ src/main/java/com/library/demo/controller/ReaderController.java
@@ -1,5 +1,6 @@
 package com.library.demo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ReaderController {
 
     @GetMapping("/users")
-    public String getUserPage(Model model){
+    public String getUserPage(@AuthenticationPrincipal(expression = "username") String username){
+//        System.out.println(username);
 //        Iterable<User> users = userRepository.findAll();
 //        model.addAttribute("users", users);
         return "userPage";
     }
+
+    @GetMapping("/addReview")
+    public String getAddReviewPage(@AuthenticationPrincipal(expression = "username") String username){
+        System.out.println(username);
+//        Iterable<User> users = userRepository.findAll();
+//        model.addAttribute("users", users);
+        return "addReview";
+    }
+
+
 }
